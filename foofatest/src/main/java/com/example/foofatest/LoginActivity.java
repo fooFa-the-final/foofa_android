8b81a762ec84cb85e75e8410f0b55c152acb119b
DESKTOP-IUCQO8M\kostacom jung <wjdgnl13@gmail.com>
2017-06-08 16:11:02 +0900

    loginActivity test중

--- foofatest/src/main/java/com/example/foofatest/LoginActivity.java
+++ foofatest/src/main/java/com/example/foofatest/LoginActivity.java
@@ -1,13 +1,94 @@
 package com.example.foofatest;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private SharedPreferences prefs;
+
+    private EditText idEdit;
+    private EditText pwEdit;
+
+    private CheckBox sellerChek;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        prefs = getSharedPreferences("loginUserId", Context.MODE_PRIVATE);
+        String id = prefs.getString("id", "");
+        if(!id.isEmpty()) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+
+        idEdit = (EditText) findViewById(R.id.idedit);
+        pwEdit = (EditText) findViewById(R.id.pwedit);
+        sellerChek = (CheckBox) findViewById(R.id.sellerCheck);
+
+        if(sellerChek.isChecked()) {
+            findViewById(R.id.loginBtn).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    new LoginCheckTask().execute("http://106.242.203.67:8088/FoodtruckFinderProject/mobilelogin.do?Id="
+                            + idEdit.getText() + "&password=" + pwEdit.getText());
+                }
+            });
+        } else {
+            findViewById(R.id.loginBtn).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    new LoginCheckTask().execute("http://106.242.203.67:8088/FoodtruckFinderProject/mobilelogin.do?Id="
+                            + idEdit.getText() + "&password=" + pwEdit.getText());
+                }
+            });
+        }
+    }
+
+    private class LoginCheckTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            HttpURLConnection http = null;
+            InputStream is = null;
+            String checkStr = null;
+            URL url = null;
+            try {
+                url = new URL(params[0]);
+                http = (HttpURLConnection) url.openConnection();
+                http.setRequestMethod("GET");
+                http.connect();
+                is = http.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+                //인코딩
+                checkStr = reader.readLine();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return checkStr;
+        }
     }
 }
