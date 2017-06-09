f0661dfb1c2f05c0de87de36656f10aa08556a1e
Hyojin Kim <hg9829@gmail.com>
2017-06-09 15:47:43 +0900

    member favorite List

---
+++ foofatest/src/main/java/com/example/foofatest/MemberFavListActivity.java
@@ -0,0 +1,13 @@
+package com.example.foofatest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MemberFavListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member_fav_list);
+    }
+}
