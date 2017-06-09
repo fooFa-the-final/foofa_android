7ab87b522ce3b1369d9fccbcb2ac2771943834b1
Hyojin Kim <hg9829@gmail.com>
2017-06-09 14:46:06 +0900

    main activity

--- foofatest/src/main/java/com/example/foofatest/MainActivity.java
+++ foofatest/src/main/java/com/example/foofatest/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.foofatest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner)findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> sAdapter = ArrayAdapter.createFromResource(this, R.array.sort, android.R.layout.simple_spinner_item);
     }
 }
