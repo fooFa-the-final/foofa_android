81b9354cdcec60775500a9192f6c4f171276a83b
zbvld <zbvld6@naver.com>
2017-06-10 14:23:54 +0900

    dggfd

--- foofatest/src/main/java/com/example/foofatest/TruckDetailActivity.java
+++ foofatest/src/main/java/com/example/foofatest/TruckDetailActivity.java
@@ -9,5 +9,8 @@ public class TruckDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_truck_detail);
+
+
+
     }
 }
