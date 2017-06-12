7d522e2eef07497ea2b5546145e4c707b6dce9b3
DESKTOP-IUCQO8M\kostacom jung <wjdgnl13@gmail.com>
2017-06-12 09:45:58 +0900

    임시

--- foofatest/src/main/java/com/example/foofatest/TruckInfoActivity.java
+++ foofatest/src/main/java/com/example/foofatest/TruckInfoActivity.java
@@ -56,7 +56,6 @@ public class TruckInfoActivity extends AppCompatActivity {
 
         TabHost tabHost1 = (TabHost) findViewById(R.id.tabHost1);
         tabHost1.setup();
-//        prefs = getSharedPreferences("loginUserId", Context.MODE_PRIVATE);
 
         TabHost.TabSpec ts1 = tabHost1.newTabSpec("Tab Spec 1");
         ts1.setContent(R.id.truckInfo1);
@@ -101,95 +100,95 @@ public class TruckInfoActivity extends AppCompatActivity {
 
 
         truckStatus = (Button)findViewById(R.id.truckStatus);
-//        truckStatus.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(TruckInfoActivity.this, TruckModifyActivity.class);
-//                intent.putExtra("foodtruck", foodtrucks.get(0));
-//                startActivity(intent);
-//            }
-//        });
+        truckStatus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TruckInfoActivity.this, TruckModifyActivity.class);
+                intent.putExtra("foodtruck", foodtrucks.get(0));
+                startActivity(intent);
+            }
+        });
 
     }
 
 
 
-//    public class FoodtruckDetailTask extends AsyncTask<String, Void, Void> {
-//
-//        @Override
-//        protected Void doInBackground(String... params) {
-//            try {
-//                URL url = new URL((String) params[0]);
-//                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-//                DocumentBuilder builder = factory.newDocumentBuilder();
-//                Document doc = builder.parse(new InputSource(url.openStream()));
-//                NodeList nodeList = doc.getElementsByTagName("foodtruck");
-//                for (int i = 0; i < nodeList.getLength(); i++) {
-//                    Foodtruck foodtruck = new Foodtruck();
-//                    Node node = nodeList.item(i);
-//                    Element element = (Element) node;
-//                    foodtruck.setFoodtruckId(getTagValue("foodtruckId", element));
-//                    foodtruck.setSellerId(getTagValue("sellerId", element));
-//                    foodtruck.setFoodtruckName(getTagValue("foodtruckName", element));
-//                    foodtruck.setOperationTime(getTagValue("operationTime", element));
-//                    foodtruck.setSpot(getTagValue("spot", element));
-//                    foodtruck.setNotice(getTagValue("notice", element));
-//                    foodtruck.setLocation(getTagValue("location", element));
-//                    foodtruck.setCategory1(getTagValue("category1", element));
-////                    foodtruck.setCategory2(getTagValue("category2", element));
-////                    foodtruck.setCategory3(getTagValue("category3", element));
-//                    foodtruck.setCard(getTagBoolean("card", element));
-//                    foodtruck.setParking(getTagBoolean("parking", element));
-//                    foodtruck.setDrinking(getTagBoolean("drinking", element));
-//                    foodtruck.setCatering(getTagBoolean("catering", element));
-//                    foodtruck.setState(getTagBoolean("state", element));
-//                    foodtruck.setFavoriteCount(Integer.parseInt(getTagValue("favoriteCount", element)));
-//                    foodtruck.setReviewCount(Integer.parseInt(getTagValue("reviewCount", element)));
-//                    foodtruck.setScore(Double.parseDouble(getTagValue("score", element)));
-//                    NodeList nodeList1 = doc.getElementsByTagName("menus");
-//                    List<Menu> menus1 = new ArrayList<>();
-//                    for(int k = 0 ; k < nodeList1.getLength(); k++) {
-//                        Menu menu = new Menu();
-//                        menu.setFoodtruckId(getTagValue("menumenuId", element));
-//                        menu.setMenuName(getTagValue("menuName", element));
-//                        menu.setPrice(Integer.parseInt(getTagValue("favoriteCount", element)));
-//                        menu.setMenuState(getTagBoolean("menuState", element));
-//                        menu.setFoodtruckId(getTagValue("menuId", element));
-//                        menus1.add(menu);
-//                    }
-//                    foodtruck.setMenus(menus1);
-//                    foodtruck.setFoodtruckImg("http://foofa.crabdance.com:8888/FoodtruckFinderProject/resources/img/food/"+getTagValue("foodtruckImg", element));
-//                }
-//            } catch (MalformedURLException e) {
-//                e.printStackTrace();
-//            } catch (ParserConfigurationException e) {
-//                e.printStackTrace();
-//            } catch (SAXException e) {
-//                e.printStackTrace();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//            return null;
-//        }
-//
-//        @Override
-//        protected void onPostExecute(Void aVoid) {
-//            adapter.notifyDataSetChanged();
-//        }
-//    }
-//
-//    private static String getTagValue(String tag, Element element) {
-//        NodeList nodeList = element.getElementsByTagName(tag).item(0).getChildNodes();
-//        Node value = (Node) nodeList.item(0);
-//        return value.getNodeValue();
-//    }
-//
-//
-//    private static Boolean getTagBoolean(String tag, Element element) {
-//        NodeList nodeList = element.getElementsByTagName(tag).item(0).getChildNodes();
-//        Node value = (Node) nodeList.item(0);
-//        return value.hasChildNodes();
-////되는지 모르겠따;
-//    }
+    public class FoodtruckDetailTask extends AsyncTask<String, Void, Void> {
+
+        @Override
+        protected Void doInBackground(String... params) {
+            try {
+                URL url = new URL((String) params[0]);
+                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder builder = factory.newDocumentBuilder();
+                Document doc = builder.parse(new InputSource(url.openStream()));
+                NodeList nodeList = doc.getElementsByTagName("foodtruck");
+                for (int i = 0; i < nodeList.getLength(); i++) {
+                    Foodtruck foodtruck = new Foodtruck();
+                    Node node = nodeList.item(i);
+                    Element element = (Element) node;
+                    foodtruck.setFoodtruckId(getTagValue("foodtruckId", element));
+                    foodtruck.setSellerId(getTagValue("sellerId", element));
+                    foodtruck.setFoodtruckName(getTagValue("foodtruckName", element));
+                    foodtruck.setOperationTime(getTagValue("operationTime", element));
+                    foodtruck.setSpot(getTagValue("spot", element));
+                    foodtruck.setNotice(getTagValue("notice", element));
+                    foodtruck.setLocation(getTagValue("location", element));
+                    foodtruck.setCategory1(getTagValue("category1", element));
+//                    foodtruck.setCategory2(getTagValue("category2", element));
+//                    foodtruck.setCategory3(getTagValue("category3", element));
+                    foodtruck.setCard(getTagBoolean("card", element));
+                    foodtruck.setParking(getTagBoolean("parking", element));
+                    foodtruck.setDrinking(getTagBoolean("drinking", element));
+                    foodtruck.setCatering(getTagBoolean("catering", element));
+                    foodtruck.setState(getTagBoolean("state", element));
+                    foodtruck.setFavoriteCount(Integer.parseInt(getTagValue("favoriteCount", element)));
+                    foodtruck.setReviewCount(Integer.parseInt(getTagValue("reviewCount", element)));
+                    foodtruck.setScore(Double.parseDouble(getTagValue("score", element)));
+                    NodeList nodeList1 = doc.getElementsByTagName("menus");
+                    List<Menu> menus1 = new ArrayList<>();
+                    for(int k = 0 ; k < nodeList1.getLength(); k++) {
+                        Menu menu = new Menu();
+                        menu.setFoodtruckId(getTagValue("menumenuId", element));
+                        menu.setMenuName(getTagValue("menuName", element));
+                        menu.setPrice(Integer.parseInt(getTagValue("favoriteCount", element)));
+                        menu.setMenuState(getTagBoolean("menuState", element));
+                        menu.setFoodtruckId(getTagValue("menuId", element));
+                        menus1.add(menu);
+                    }
+                    foodtruck.setMenus(menus1);
+                    foodtruck.setFoodtruckImg("http://foofa.crabdance.com:8888/FoodtruckFinderProject/resources/img/food/"+getTagValue("foodtruckImg", element));
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (ParserConfigurationException e) {
+                e.printStackTrace();
+            } catch (SAXException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+    private static String getTagValue(String tag, Element element) {
+        NodeList nodeList = element.getElementsByTagName(tag).item(0).getChildNodes();
+        Node value = (Node) nodeList.item(0);
+        return value.getNodeValue();
+    }
+
+
+    private static Boolean getTagBoolean(String tag, Element element) {
+        NodeList nodeList = element.getElementsByTagName(tag).item(0).getChildNodes();
+        Node value = (Node) nodeList.item(0);
+        return value.hasChildNodes();
+//되는지 모르겠따;
+    }
 
 }
