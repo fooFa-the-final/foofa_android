a43a84dc86dcccb575cbcc66fc3dd376266eae36
DESKTOP-IUCQO8M\kostacom jung <wjdgnl13@gmail.com>
2017-06-14 12:40:21 +0900

    백업

--- foofatest/src/main/java/com/example/foofatest/ButtonListActivity.java
+++ foofatest/src/main/java/com/example/foofatest/ButtonListActivity.java
@@ -1,10 +1,36 @@
 package com.example.foofatest;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.example.foofatest.dto.Foodtruck;
+import com.example.foofatest.dto.Member;
+import com.example.foofatest.dto.Menu;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.lang.ref.WeakReference;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 public class ButtonListActivity extends AppCompatActivity {
 
@@ -54,5 +80,109 @@ public class ButtonListActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+
+
     }
+
+
+
+    public class MenuDetailTask extends AsyncTask<String, Void, Void> {
+
+        Member member ;
+
+
+
+        @Override
+        protected Void doInBackground(String... params) {
+            try {
+                URL url = new URL((String) params[0]);
+                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder builder = factory.newDocumentBuilder();
+                Document doc = builder.parse(new InputSource(url.openStream()));
+                NodeList nodeList = doc.getElementsByTagName("member");
+                Member member = new Member();
+                    Node node = nodeList.item(0);
+                    Element element = (Element) node;
+                    member.setMemberId(getTagValue("memberId", element));
+                    member.setBirthday(getTagValue("birthday", element));
+                    member.setFollowCount(Integer.parseInt(getTagValue("followCount", element)));
+                member.setReviewCount(Integer.parseInt(getTagValue("reviewCount", element)));
+                member.setProfileImg("http://foofa.crabdance.com:8888/FoodtruckFinderProject/resources/img/upload/" + getTagValue("profileImg", element));
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (ParserConfigurationException e) {
+                e.printStackTrace();
+            } catch (SAXException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+    }
+
+    private static String getTagValue(String tag, Element element) {
+        try {
+            NodeList list = element.getElementsByTagName(tag).item(0).getChildNodes();
+            Node value = (Node) list.item(0);
+            return value.getNodeValue();
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    private class ImageLoadingTask extends AsyncTask<String, Void, Bitmap> {
+
+        private final WeakReference<ImageView> imageViewWeakReference;
+
+        public ImageLoadingTask(ImageView img) {
+            this.imageViewWeakReference = new WeakReference<ImageView>(img);
+        }
+
+
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            URL url = null;
+
+            try {
+                url = new URL(params[0]);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            return getRemoteImage(url);
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            if (isCancelled()) {
+                bitmap = null;
+            }
+            if (imageViewWeakReference != null) {
+                ImageView imageView = imageViewWeakReference.get();
+                if (imageView != null) {
+                    imageView.setImageBitmap(bitmap);
+                }
+            }
+        }
+    }
+
+    private Bitmap getRemoteImage(final URL url) {
+        Bitmap bitmap = null;
+        URLConnection conn;
+        try {
+            conn = url.openConnection();
+            conn.connect();
+            BufferedInputStream bis = new BufferedInputStream(conn.getInputStream());
+            bitmap = BitmapFactory.decodeStream(bis);
+            bis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
+
 }
--- foofatest/src/main/java/com/example/foofatest/MemberProfileActivity.java
+++ foofatest/src/main/java/com/example/foofatest/MemberProfileActivity.java
@@ -1,13 +1,119 @@
 package com.example.foofatest;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.foofatest.dto.Foodtruck;
+import com.example.foofatest.dto.Menu;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.lang.ref.WeakReference;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 public class MemberProfileActivity extends AppCompatActivity {
+
+
+    public ImageView myProfile;
+    public TextView myId;
+    public TextView myEmail;
+    public TextView memberFollowerCount;
+    public TextView memberFavCount;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member_profile);
+
+
+        myProfile.findViewById(R.id.myProfile);
+        myId.findViewById(R.id.myId);
+        myEmail.findViewById(R.id.myEmail);
+        memberFollowerCount.findViewById(R.id.memberFollowerCount);
+        memberFavCount.findViewById(R.id.memberFavCount);
+
+        myProfile.setTag("myId");
+//        myProfile.setImageBitmap();
+
+
+//        new ImageLoadingTask(myProfile).execute(member.getPr)
+
+
+
     }
+
+    private class ImageLoadingTask extends AsyncTask<String, Void, Bitmap> {
+
+        private final WeakReference<ImageView> imageViewWeakReference;
+
+        public ImageLoadingTask(ImageView img) {
+            this.imageViewWeakReference = new WeakReference<ImageView>(img);
+        }
+
+
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            URL url = null;
+
+            try {
+                url = new URL(params[0]);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            return getRemoteImage(url);
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            if (isCancelled()) {
+                bitmap = null;
+            }
+            if (imageViewWeakReference != null) {
+                ImageView imageView = imageViewWeakReference.get();
+                if (imageView != null) {
+                    imageView.setImageBitmap(bitmap);
+                }
+            }
+        }
+    }
+
+    private Bitmap getRemoteImage(final URL url) {
+        Bitmap bitmap = null;
+        URLConnection conn;
+        try {
+            conn = url.openConnection();
+            conn.connect();
+            BufferedInputStream bis = new BufferedInputStream(conn.getInputStream());
+            bitmap = BitmapFactory.decodeStream(bis);
+            bis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
+
+
+
 }
